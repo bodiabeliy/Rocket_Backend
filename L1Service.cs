c4e8e44c4999c977a4e673c194fb9eb49998b2ae
bodiabeliy <bboda2106@gmail.com>
2023-01-05 20:39:55 +0200

    applied recomandations: removing unusable lines code, custom conditions;naming fixing

--- L1Service.cs
+++ L1Service.cs
@@ -10,13 +10,7 @@ using System.Linq;
 public class L1Service
 {
     private readonly IOrganizationService _service;
-    private readonly string FIRST_STAGE = "First stage";
-    private readonly string SECOND_STAGE = "Second stage";
-    private readonly string INTER_STAGE = "Inter stage";
-    private readonly string FAIRING = "Fairing";
-
-
-    public string outputJSON = "";
+    private string outputJSON = "";
 
     Rocket rocket = new Rocket();
     L2 l2Rocket = new L2();
@@ -38,113 +32,45 @@ public class L1Service
 
     private Rocket GetRocket(Guid l1Id)
     {
-        EntityCollection l1ItemsResponse;
-        do
-        {
-            var query = new QueryExpression("env_part") { ColumnSet = new ColumnSet("env_partid", "env_name", "env_description", "env_imageurl") };
-            query.Criteria.AddCondition(new ConditionExpression("env_partid", ConditionOperator.Equal, l1Id.ToString()));
-            l1ItemsResponse = _service.RetrieveMultiple(query);
-        } while (l1ItemsResponse.MoreRecords);
-        string related = "";
-        foreach (var entityElement in l1ItemsResponse.Entities)
-        {
-            related = (entityElement.Attributes["env_name"]).ToString();
+        var parts = _service.Retrieve("env_part", l1Id, new ColumnSet("env_name", "env_description", "env_imageurl"));
 
-            rocket.Name = (entityElement.Attributes["env_name"]).ToString();
-            rocket.Description = (entityElement.Attributes["env_description"]).ToString();
-            rocket.ImageUrl = (entityElement.Attributes["env_imageurl"]).ToString();
-            entityElement.Attributes.Add("env_jsonschema", "test value l2");
-            _service.Update(entityElement);
-
-            GetL2Items(rocket.L2Items);
-        }
-
+        rocket.Name = (parts.Attributes["env_name"]).ToString();
+        rocket.Description = (parts.Attributes["env_description"]).ToString();
+        rocket.ImageUrl = (parts.Attributes["env_imageurl"]).ToString();
+        GetL2Items(rocket.L2Items, (parts.Attributes["env_name"]).ToString());
         return rocket;
     }
 
-
     private void UpdateRecordJson(Guid l1Id)
     {
-        EntityCollection l1ItemsResponse;
-        do
-        {
-            var query = new QueryExpression("env_part") { ColumnSet = new ColumnSet("env_partid", "env_name", "env_description", "env_imageurl") };
-            query.Criteria.AddCondition(new ConditionExpression("env_partid", ConditionOperator.Equal, l1Id.ToString()));
-            l1ItemsResponse = _service.RetrieveMultiple(query);
-        } while (l1ItemsResponse.MoreRecords);
-        foreach (var entityElement in l1ItemsResponse.Entities)
-        {
+        var parts = _service.Retrieve("env_part", l1Id, new ColumnSet("env_name", "env_description", "env_imageurl"));
 
-            entityElement.Attributes.Add("env_jsonschema", outputJSON);
-            _service.Update(entityElement);
-
-        }
-
+        rocket.Name = (parts.Attributes["env_name"]).ToString();
+        rocket.Description = (parts.Attributes["env_description"]).ToString();
+        rocket.ImageUrl = (parts.Attributes["env_imageurl"]).ToString();
+        GetL2Items(rocket.L2Items, (parts.Attributes["env_name"]).ToString());
     }
 
-
-
-
-    private List<L2> GetL2Items(List<L2> l2ItemsList)
+    private List<L2> GetL2Items(List<L2> l2ItemsList, string l1ParentName)
     {
         EntityCollection l2ItemsResponse;
         do
         {
-            var query = new QueryExpression("env_l2") { ColumnSet = new ColumnSet("env_name", "cr273_description", "env_l1itemid", "cr273_imageurl") };
+            var query = new QueryExpression("env_l2") { ColumnSet = new ColumnSet("env_name", "cr273_description", "env_l1itemid", "cr273_imageurl", "env_l1items") };
+            query.Criteria.AddCondition(new ConditionExpression("env_l1items", ConditionOperator.Equal, l1ParentName));
             l2ItemsResponse = _service.RetrieveMultiple(query);
 
         } while (l2ItemsResponse.MoreRecords);
         var groupedL2Items = l2ItemsResponse.Entities.GroupBy(l2Item => l2Item.Attributes["env_name"].ToString());
+        string[] l2ParentGroupNames = groupedL2Items.Select(l2ParentGroupName => Convert.ToString(l2ParentGroupName.Key)).ToArray();
+
         foreach (var group in groupedL2Items)
         {
-            var currentGroupName = group.Key;
-            String[] l2ParentGroupNames = groupedL2Items.Select(l2ParentGroupName => {
-                return Convert.ToString(l2ParentGroupName.Key);
-            }).ToArray();
-
             foreach (var groupedItem in group)
             {
-                Console.WriteLine("currentGroupName: " + currentGroupName);
-                if (groupedItem.Attributes["env_name"].ToString() == FIRST_STAGE)
+                string relatedName = groupedItem.Attributes["env_name"].ToString();
+                if (relatedName == group.Key)
                 {
-                    string relatedName = groupedItem.Attributes["env_name"].ToString();
-
-                    l2ItemsList.Add(new L2
-                    {
-                        Name = groupedItem.Attributes["env_name"].ToString(),
-                        Description = groupedItem.Attributes["cr273_description"].ToString(),
-                        ImageUrl = groupedItem.Attributes["cr273_imageurl"].ToString(),
-                        L3Items = GetL3Items(l2Rocket.L3Items, relatedName, l2ParentGroupNames)
-                    });
-                }
-                if (groupedItem.Attributes["env_name"].ToString() == SECOND_STAGE)
-                {
-                    string relatedName = groupedItem.Attributes["env_name"].ToString();
-                    L2 l2Rocket = new L2();
-                    l2ItemsList.Add(new L2
-                    {
-                        Name = groupedItem.Attributes["env_name"].ToString(),
-                        Description = groupedItem.Attributes["cr273_description"].ToString(),
-                        ImageUrl = groupedItem.Attributes["cr273_imageurl"].ToString(),
-                        L3Items = GetL3Items(l2Rocket.L3Items, relatedName, l2ParentGroupNames)
-                    });
-                }
-                if (groupedItem.Attributes["env_name"].ToString() == INTER_STAGE)
-                {
-                    string relatedName = groupedItem.Attributes["env_name"].ToString();
-                    L2 l2Rocket = new L2();
-                    l2ItemsList.Add(new L2
-                    {
-                        Name = groupedItem.Attributes["env_name"].ToString(),
-                        Description = groupedItem.Attributes["cr273_description"].ToString(),
-                        ImageUrl = groupedItem.Attributes["cr273_imageurl"].ToString(),
-                        L3Items = GetL3Items(l2Rocket.L3Items, relatedName, l2ParentGroupNames)
-                    });
-                }
-                if (groupedItem.Attributes["env_name"].ToString() == FAIRING)
-                {
-                    string relatedName = groupedItem.Attributes["env_name"].ToString();
-                    L2 l2Rocket = new L2();
                     l2ItemsList.Add(new L2
                     {
                         Name = groupedItem.Attributes["env_name"].ToString(),
@@ -155,47 +81,45 @@ public class L1Service
                 }
             }
 
-
         }
-        return l2ItemsList;
-
+        return new List<L2>(l2ItemsList);
     }
 
     private List<L3> GetL3Items(List<L3> l3ItemsList, string currentParentGroup, string[] l2ParentGroupNames)
     {
-        foreach (var l2ParentGroupName in l2ParentGroupNames.Select((currentGroupName) => new { currentGroupName }))
+        l3ItemsList.Clear();
+        EntityCollection l3ItemsResponse;
+        do
+        {
+            var query = new QueryExpression("env_l3items")
+            {
+                ColumnSet = new ColumnSet(
+                "env_name",
+                "env_description",
+                "env_categories",
+                "env_imageurl",
+                "env_svgxml",
+                "env_svgwidth",
+                "env_svgheight",
+                "env_svgviewbox",
+                "env_svgpreserveaspectratio",
+                "env_svgschemaurl",
+                "env_parentcategory",
+                "env_modulecategory"
+                )
+            };
+            query.Criteria.AddCondition(new ConditionExpression("env_parentcategory", ConditionOperator.Equal, currentParentGroup));
+            l3ItemsResponse = _service.RetrieveMultiple(query);
+
+        } while (l3ItemsResponse.MoreRecords);
+        foreach (var l2ParentGroupName in l2ParentGroupNames.Select((currentL3GroupName) => new { currentL3GroupName }))
         {
 
-            if (currentParentGroup == l2ParentGroupName.currentGroupName)
+            if (currentParentGroup == l2ParentGroupName.currentL3GroupName)
             {
-                EntityCollection l3ItemsResponse;
-                do
-                {
-                    var query = new QueryExpression("env_l3items")
-                    {
-                        ColumnSet = new ColumnSet(
-                        "env_name",
-                        "env_description",
-                        "env_categories",
-                        "env_imageurl",
-                        "env_svgxml",
-                        "env_svgwidth",
-                        "env_svgheight",
-                        "env_svgviewbox",
-                        "env_svgpreserveaspectratio",
-                        "env_svgschemaurl",
-                        "env_parentcategory",
-                        "env_modulecategory"
-                        )
-                    };
-                    query.Criteria.AddCondition(new ConditionExpression("env_parentcategory", ConditionOperator.Equal, currentParentGroup));
-                    l3ItemsResponse = _service.RetrieveMultiple(query);
-
-                } while (l3ItemsResponse.MoreRecords);
                 var sortedL3Items = l3ItemsResponse.Entities.OrderByDescending(l3Item => l3Item.Attributes["env_name"].ToString()).ToArray();
                 foreach (var entityElement in sortedL3Items)
                 {
-
                     l3ItemsList.Add(new L3
                     {
                         Name = entityElement.Attributes["env_name"].ToString(),
@@ -207,7 +131,6 @@ public class L1Service
                         SvgViewBox = entityElement.Attributes["env_svgviewbox"].ToString(),
                         SvgPreserveAspectRatio = entityElement.Attributes["env_svgpreserveaspectratio"].ToString(),
                         SvgSchemaJsonUrl = entityElement.Attributes["env_svgschemaurl"].ToString(),
-
                         Category = entityElement.Attributes["env_modulecategory"].ToString(),
                         ParentCategory = entityElement.Attributes["env_parentcategory"].ToString()
 
@@ -217,14 +140,40 @@ public class L1Service
                 }
             }
         }
-        return l3ItemsList;
+        return new List<L3>(l3ItemsList);
 
     }
 
-    private void UploadToAzure(string connection, string container, string blob, string updatedJSON)
+    // It is a good practice to retrieve different entities in different providers like:
+
+    /*class BaseProvider
     {
-        
+        protected readonly IOrganizationService OrganizationService;
+
+        public readonly string EntityName;
+
+        public BaseProvider(IOrganizationService organizationService, string entityName)
+        {
+            OrganizationService = organizationService;
+            EntityName = entityName;
+        }
+
+        protected Entity GetById(Guid id, ColumnSet columnSet = null)
+        {
+            columnSet ??= new ColumnSet(false);
+            return OrganizationService.Retrieve(EntityName, id, columnSet);
+        }
     }
 
+    class ChildEntityProvider : BaseProvider
+    {
+        public ChildEntityProvider(IOrganizationService organizationService, string entityName) : base(organizationService, entityName)
+        {
+        }
 
+        public Entity GetById(Guid id)
+        {
+            return GetById(id, new ColumnSet("fieldname"));
+        }
+    }*/
 }
--- RetrievePlugin.cs
+++ RetrievePlugin.cs
@@ -10,11 +10,7 @@ namespace RetrievePlugin
     {
         IOrganizationService service;
         IPluginExecutionContext context;
-        ITracingService trace;
 
-        private readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=rocketapp;AccountKey=mdFGM01bT4lSHLi+aBaARyKv39qimifmlvhkMoHEWBpANgFVdy6rjVDrkZQarcoAXs636g3F4Or1+AStGU+xfg==;EndpointSuffix=core.windows.net";
-        private readonly string containerName = "json";
-        private readonly string blobName = "RetrievedDataFromCRM.json";
 
         // registration Plugin in CRM
         public void GetOrganizationService(IServiceProvider serviceProvider)
@@ -23,12 +19,11 @@ namespace RetrievePlugin
             service = ((IOrganizationServiceFactory)serviceProvider
                 .GetService(typeof(IOrganizationServiceFactory)))
                 .CreateOrganizationService(context.UserId);
-            trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            L1Service _service = new L1Service(service);
+            var _service = new L1Service(service);
 
-            string JSON = _service.GetData((Guid)context.PrimaryEntityId);
+            string recievedJson = _service.GetData(context.PrimaryEntityId);
             var l1 = context.OutputParameters["BusinessEntity"] as Entity;
-            l1["env_jsonschema"] = JSON;
+            l1["env_jsonschema"] = recievedJson;
         }
 
 
